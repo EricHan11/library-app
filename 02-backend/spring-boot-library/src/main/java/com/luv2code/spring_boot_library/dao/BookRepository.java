dfce0a5af1382abc8898b5091763f6aeb7e53400
EricHan11 <60022539+EricHan11@users.noreply.github.com>
2026-03-13 18:29:09 -0500

    Delete 02-backend directory
    Renaming it to Backend

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/dao/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.luv2code.spring_boot_library.dao;
-
-import com.luv2code.spring_boot_library.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
