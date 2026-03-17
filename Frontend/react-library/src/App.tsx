f7eab5830f6048a062bda27b13f0b4e8fc458294
Eric Han <ehan2019@gmail.com>
2026-03-16 22:23:04 -0500

    Added page routing. Now you can switch pages by clicking various links or visiting the links and homepage/searchBookPage will render respectively. Fixed Footer so it will always display at bottom of screen.

--- Frontend/react-library/src/App.tsx
+++ Frontend/react-library/src/App.tsx
@@ -4,15 +4,31 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
+  //the divs make sure the Footer component stays at the bottom of screen
+  //Ex: when no results for searched book, footer should stay at bottom
+  //flex-grow-1 makes the div stretch to rest of screen where there's space
+  //Ex:Full page is 1000px, Navbar is 100px, Footer is 100px, div will take 800px.
+  //The contents will sit inside the div or make the div stretch to fit it
   return (
-    <div>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home'>
+            <HomePage />
+          </Route>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
-    
   );
 };
--- Frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ Frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 import BookModel from '../../models/BookModel';
 import { SpinnerLoading } from '../Utils/SpinnerLoading';
 import { SearchBook } from './components/SearchBook';
